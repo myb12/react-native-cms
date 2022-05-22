cd4629bcc6590587e58eeb902769d53c4d2f21ae
myb12 <mohammadyasinbappy@outlook.com>
2022-05-22 14:18:37 +0600

    Stack and drawer navigator added

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
 
-export default function App() {
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';;
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './screens/HomeScreen';
+import NewTweet from './screens/NewTweet';
+import TweetScreen from './screens/TweetScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack.Navigator>
+      <Stack.Screen name="Home1" component={HomeScreen} />
+      <Stack.Screen name="New Tweet" component={NewTweet} />
+      <Stack.Screen name="Tweet Screen" component={TweetScreen} />
+      <Stack.Screen name="Profile Screen" component={ProfileScreen} />
+    </Stack.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name="Home" component={HomeStackNavigator} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
