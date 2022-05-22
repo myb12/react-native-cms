53065621ff632cfade4d2d69038596727273d260
myb12 <mohammadyasinbappy@outlook.com>
2022-05-23 02:07:13 +0600

    Implemented home screen design with tweet item flatlist

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,181 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, Platform } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import React from 'react'
 
 export default function HomeScreen({ navigation }) {
+
+    const gotoProfile = () => {
+        navigation.navigate('Profile Screen');
+    }
+    const gotoSingleTweet = () => {
+        navigation.navigate('Tweet Screen');
+    }
+
+    const gotoNewTweet = () => {
+        navigation.navigate('New Tweet');
+    }
+
+    const DATA = [
+        {
+            id: '1',
+            title: 'First Item',
+        },
+        {
+            id: '2',
+            title: 'Second Item',
+        },
+        {
+            id: '3',
+            title: 'Third Item',
+        },
+        {
+            id: '4',
+            title: 'Fourth Item',
+        },
+        {
+            id: '5',
+            title: 'Fifth Item',
+        },
+        {
+            id: '6',
+            title: 'Sixth Item',
+        },
+        {
+            id: '7',
+            title: 'Seventh Item',
+        },
+        {
+            id: '8',
+            title: 'Eight Item',
+        },
+        {
+            id: '9',
+            title: 'Ninth Item',
+        },
+        {
+            id: '10',
+            title: 'Tenth Item',
+        },
+    ];
+
+
+    const renderItem = ({ item }) => (
+        <View style={styles.tweetContainer}>
+            <TouchableOpacity onPress={() => gotoProfile()}>
+                <Image style={styles.avatar} source={{
+                    uri: 'https://reactnative.dev/img/tiny_logo.png'
+                }} />
+            </TouchableOpacity>
+            <View style={{ flex: 1 }}>
+                <TouchableOpacity style={styles.flexRow} onPress={() => gotoSingleTweet()}>
+                    <Text numberOfLines={1} style={styles.tweetName}>{item.title}</Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>@yasin</Text>
+                    <Text>&middot;</Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>9m</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.tweetContentContainer} onPress={() => gotoSingleTweet()}>
+                    <Text style={styles.tweetContent}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim minima dicta rem fugiat pariatur neque nesciunt doloribus iusto consectetur ad.</Text>
+                </TouchableOpacity>
+
+                <View style={styles.tweetEngagement}>
+                    <TouchableOpacity style={styles.flexRow}>
+                        <EvilIcons name="comment" size={22} color="gray" style={{ marginRight: 2 }} />
+                        <Text style={styles.textGray}>456</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                        <EvilIcons name="retweet" size={22} color="gray" style={{ marginRight: 2 }} />
+                        <Text style={styles.textGray}>456</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                        <EvilIcons name="heart" size={22} color="gray" style={{ marginRight: 2 }} />
+                        <Text style={styles.textGray}>2,456</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                        <EvilIcons name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'} size={22} color="gray" style={{ marginRight: 2 }} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>HomeScreen / Feed</Text>
-            <Button title="New Tweet" onPress={() => navigation.navigate('New Tweet')} />
-            <Button title="Tweet Screen" onPress={() => navigation.navigate('Tweet Screen')} />
-            <Button title="Profile Screen" onPress={() => navigation.navigate('Profile Screen')} />
+        <View style={styles.container}>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={() => <View style={styles.tweetSeperator}></View>}
+            />
+
+            <TouchableOpacity style={styles.floatingButton} onPress={() => gotoNewTweet()}>
+                <AntDesign name="plus" size={24} color="white" />
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    flexRow: {
+        flexDirection: 'row',
+    },
+    tweetContainer: {
+        flexDirection: 'row',
+        paddingHorizontal: 12,
+        paddingVertical: 12
+    },
+    tweetSeperator: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#e5e7eb',
+    },
+    avatar: {
+        width: 42,
+        height: 42,
+        marginRight: 8,
+        borderRadius: 21
+    },
+    tweetName: {
+        fontWeight: 'bold',
+        color: '#222222',
+    },
+    tweetHandle: {
+        color: 'gray',
+        marginHorizontal: 4
+    },
+    tweetContentContainer: {
+        marginTop: 4,
+    },
+    tweetContent: {
+        lineHeight: 22
+    },
+    textGray: {
+        color: 'gray',
+    },
+    tweetEngagement: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 12
+    },
+    ml4: {
+        marginLeft: 16
+    },
+    floatingButton: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1d9bf1',
+        position: 'absolute',
+        bottom: 20,
+        right: 12
+    }
+})
